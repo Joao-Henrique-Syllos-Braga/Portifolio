56c935e568801bc3304e478df7d51ad9fcac7709
Syllos <joaohenrique.braga@outlook.com>
2025-12-11 23:25:19 -0300

    Hero, Sobre feitos

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggle = document.getElementById("toggle");
+const toggleBall = document.getElementById("toggle-ball");
+
+toggle.addEventListener("click", () => {
+  toggle.classList.toggle("active");
+});
